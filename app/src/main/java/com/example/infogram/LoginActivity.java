6e134bf9855b0cf8c7634a0bf4a4a96db8adbc2b
EgCooper <vicox30@gmail.com>
2021-09-16 01:25:43 -0400

    INFOGRAM10

--- app/src/main/java/com/example/infogram/LoginActivity.java
+++ app/src/main/java/com/example/infogram/LoginActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.infogram.view.ContainerActivity;
+import com.example.infogram.view.CreateAccountActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -14,7 +17,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void goCreateAccount(View view){
-        Intent intent = new Intent(this,CreateAccountActivity.class);
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent= new Intent(this, ContainerActivity.class);
         startActivity(intent);
     }
 }
