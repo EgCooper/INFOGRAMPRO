f63433e169e368fa75f6d2bf7c62edc69b0f1151
EgCooper <vicox30@gmail.com>
2021-09-23 18:57:58 -0400

    INFOGRAM16

--- app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/infogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.example.infogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.infogram.R;
+import com.example.infogram.adapter.CardViewAdapter;
+import com.example.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +68,35 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false,view);
+
+        RecyclerView recyclerView=view.findViewById(R.id.recycler_view_profile);
+
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        CardViewAdapter cardviewAdapter=new CardViewAdapter(buidImages(),R.layout.cardview_image,getActivity());
+        recyclerView.setAdapter(cardviewAdapter);
+        return view;
+
     }
+    public void showToolbar(String titulo,boolean botonSubir, View view){
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image> images = new ArrayList<>();
+        images.add(new Image("https://images.wallpaperscraft.com/image/single/fox_stick_autumn_221120_1280x720.jpg","Carlos","1","2"));
+        images.add(new Image("https://images.wallpaperscraft.com/image/single/fox_stick_autumn_221120_1280x720.jpg","Victor","2","5"));
+        images.add(new Image("https://images.wallpaperscraft.com/image/single/fox_stick_autumn_221120_1280x720.jpg","Andres","4","10"));
+        images.add(new Image("https://images.wallpaperscraft.com/image/single/fox_stick_autumn_221120_1280x720.jpg","Carla","3","20"));
+        System.out.println(images);
+
+        return images;
+    }
+
 }
